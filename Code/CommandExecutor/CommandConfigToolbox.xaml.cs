650a1fcb482d9117851f5bc60d066f3b6bfc7504
吕毅 <lvyi@cvte.cn>
2014-12-23 20:56:25 +0800

    Add basic solution files

---
+++ Code/CommandExecutor/CommandConfigToolbox.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Walterlv.VisualStudio.CommandExecutor
+{
+    [ProvideToolboxControl("Command Executor", true)]
+    public partial class CommandConfigToolbox : UserControl
+    {
+        public CommandConfigToolbox()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(string.Format(System.Globalization.CultureInfo.CurrentUICulture, "We are inside {0}.button1_Click()", this.ToString()));
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+Visual Studio Command Executor
+=====
+
+-----
+
+This is a very convenient add-on to execute command under your solution directories. You can use it for your git source control, your project dependency swiching, or any other ideas in your mind.
